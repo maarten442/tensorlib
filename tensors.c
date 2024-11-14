9de362c2e8d212dfe359e42bfdd46a9e5ca1c1f5
maarten442 <maartenrottier@live.nl>
2024-11-14 21:26:47 +0100

    added the initial structures for Function, Tensor, Gradient Tensor

--- tensors.c
+++ tensors.c
@@ -5,6 +5,8 @@
 #include <assert.h>
 
 // Create a wrapper around malloc for save memory allocation, as per Karpathy. 
+// We use fprintf to write to the standard error instead of the standard output. 
+// Program needs to terminate immediately upon failed malloc, so we use exit. 
 
 void* malloc_check(int size, char* file, int line) {
     void* ptr = malloc(size);
@@ -14,3 +16,44 @@ void* malloc_check(int size, char* file, int line) {
     }
     return ptr;
 }
+
+// define macro mallocCheck
+
+#define mallocCheck(size) malloc_check(size, __FILE__, __LINE__)
+
+// Storage physically stores the values in contiguous memory. Can be referenced by multiple tensors. 
+
+typedef struct {
+    float* data;
+    int size;
+    int ref_count;
+} Storage;
+
+typedef struct {
+    Tensor** inputs;
+    Tensor* output;
+    int num_inputs;
+    void (*backward)(struct Function* self, Tensor* output);
+    // Something to destroy resources?
+} Function;
+
+typedef struct {
+    Storage* storage;
+    int offset;
+    int stride;
+    int size;
+    char* repr;
+} GradientTensor;
+
+typedef struct {
+    Storage* storage;
+    GradientTensor* grad;
+    int offset;
+    int stride;
+    int size;
+    char* repr;
+    bool require_grad;
+    bool is_leaf;
+    Function* grad_fn;
+} Tensor;
+
